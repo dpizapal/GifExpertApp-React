7ec5ae37141eaae86eb14f4d94d661e22f8bb0e7
dpizapal <pizarro.developer@gmail.com>
2022-11-25 12:41:10 +0100

    hook useEffect to optimize apis calls

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,16 +1,20 @@
-import React from 'react'
+import {useEffect} from 'react'
 import { getGifs} from '../helpers/getGifs'
 
 
 export const GifGrid = ({category}) => {
 
-  getGifs ( category );
+  useEffect(() => {
+  
+    getGifs (category);
+    
+  }, [])
+  
 
 
   return (
     <>
       <h3>{ category }</h3>
-      <p>Hola Mundo</p>
     </>
   )
 }
