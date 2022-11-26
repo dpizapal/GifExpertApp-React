528db9baa051e5f8cd6b4a9a7e4b27b22f1d30fc
dpizapal <pizarro.developer@gmail.com>
2022-11-26 14:41:03 +0100

    create loading message

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,16 +1,20 @@
 
-import { useFetchFifs } from '../hooks/useFetchFifs';
+import { useFetchGifs } from '../hooks/useFetchFifs';
 import { GiftItem } from './GiftItem';
 
 
 export const GifGrid = ({category}) => {
 
-  const { images, isLoading } = useFetchFifs ( category );
+  const { images, isLoading } = useFetchGifs ( category );
 
 
   return (
     <>
       <h3>{ category }</h3>
+      {
+        isLoading && ( <h2>Cargando...</h2>)
+      }
+
       <div className='card-grid'>
         {
           images.map ( ( image )=> (
--- src/hooks/useFetchFifs.js
+++ src/hooks/useFetchFifs.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { getGifs } from "../helpers/getGifs";
 
 
-export const useFetchFifs = ( category ) => {
+export const useFetchGifs = ( category ) => {
 
   const [images, setImages] = useState ([]);
   const [isLoading, setIsLoading] = useState (true);
